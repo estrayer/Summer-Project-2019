8a402760c9ec64bbd358895b1d8241a3985d0083
estrayer <estrayer@users.noreply.github.com>
2019-06-05 15:40:10 -0400

    Test
    just what I was using to try to figure out how anything works.

---
+++ Testing Grounds/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+			this.DiagramsDropDownBox = new System.Windows.Forms.ToolStripDropDownButton();
+			this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+			this.arrayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.pointerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.insert_Rectangle = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+			this.UMLDropDownBox = new System.Windows.Forms.ToolStripDropDownButton();
+			this.classToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.relationshipToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+			this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+			this.drawingSurface1 = new WindowsFormsApp1.DrawingSurface();
+			this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+			this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+			this.toolStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.DiagramsDropDownBox,
+            this.toolStripSeparator1,
+            this.toolStripLabel2,
+            this.UMLDropDownBox,
+            this.toolStripSeparator2,
+            this.toolStripButton1});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(1429, 25);
+			this.toolStrip1.TabIndex = 0;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// toolStripLabel1
+			// 
+			this.toolStripLabel1.Name = "toolStripLabel1";
+			this.toolStripLabel1.Size = new System.Drawing.Size(57, 22);
+			this.toolStripLabel1.Text = "Diagrams";
+			this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
+			// 
+			// DiagramsDropDownBox
+			// 
+			this.DiagramsDropDownBox.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.DiagramsDropDownBox.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1,
+            this.arrayToolStripMenuItem,
+            this.pointerToolStripMenuItem,
+            this.insert_Rectangle});
+			this.DiagramsDropDownBox.Image = ((System.Drawing.Image)(resources.GetObject("DiagramsDropDownBox.Image")));
+			this.DiagramsDropDownBox.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.DiagramsDropDownBox.Name = "DiagramsDropDownBox";
+			this.DiagramsDropDownBox.Size = new System.Drawing.Size(49, 22);
+			this.DiagramsDropDownBox.Text = "Insert";
+			this.DiagramsDropDownBox.Click += new System.EventHandler(this.toolStripDropDownButton1_Click);
+			// 
+			// toolStripTextBox1
+			// 
+			this.toolStripTextBox1.Name = "toolStripTextBox1";
+			this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+			this.toolStripTextBox1.Text = "Tree";
+			// 
+			// arrayToolStripMenuItem
+			// 
+			this.arrayToolStripMenuItem.Name = "arrayToolStripMenuItem";
+			this.arrayToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.arrayToolStripMenuItem.Text = "Array";
+			this.arrayToolStripMenuItem.Click += new System.EventHandler(this.arrayToolStripMenuItem_Click);
+			// 
+			// pointerToolStripMenuItem
+			// 
+			this.pointerToolStripMenuItem.Name = "pointerToolStripMenuItem";
+			this.pointerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.pointerToolStripMenuItem.Text = "Pointer";
+			// 
+			// insert_Rectangle
+			// 
+			this.insert_Rectangle.Name = "insert_Rectangle";
+			this.insert_Rectangle.Size = new System.Drawing.Size(180, 22);
+			this.insert_Rectangle.Text = "Rectangle";
+			this.insert_Rectangle.Click += new System.EventHandler(this.insert_Rectangle_Click);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+			// 
+			// toolStripLabel2
+			// 
+			this.toolStripLabel2.Name = "toolStripLabel2";
+			this.toolStripLabel2.Size = new System.Drawing.Size(32, 22);
+			this.toolStripLabel2.Text = "UML";
+			// 
+			// UMLDropDownBox
+			// 
+			this.UMLDropDownBox.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.UMLDropDownBox.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.classToolStripMenuItem,
+            this.relationshipToolStripMenuItem});
+			this.UMLDropDownBox.Image = ((System.Drawing.Image)(resources.GetObject("UMLDropDownBox.Image")));
+			this.UMLDropDownBox.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.UMLDropDownBox.Name = "UMLDropDownBox";
+			this.UMLDropDownBox.Size = new System.Drawing.Size(164, 22);
+			this.UMLDropDownBox.Text = "toolStripDropDownButton1";
+			// 
+			// classToolStripMenuItem
+			// 
+			this.classToolStripMenuItem.Name = "classToolStripMenuItem";
+			this.classToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.classToolStripMenuItem.Text = "Class";
+			// 
+			// relationshipToolStripMenuItem
+			// 
+			this.relationshipToolStripMenuItem.Name = "relationshipToolStripMenuItem";
+			this.relationshipToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.relationshipToolStripMenuItem.Text = "Relationship";
+			// 
+			// toolStripSeparator2
+			// 
+			this.toolStripSeparator2.Name = "toolStripSeparator2";
+			this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+			// 
+			// toolStripButton1
+			// 
+			this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+			this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton1.Name = "toolStripButton1";
+			this.toolStripButton1.Size = new System.Drawing.Size(35, 22);
+			this.toolStripButton1.Text = "Save";
+			// 
+			// drawingSurface1
+			// 
+			this.drawingSurface1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.drawingSurface1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.drawingSurface1.Location = new System.Drawing.Point(0, 25);
+			this.drawingSurface1.Name = "drawingSurface1";
+			this.drawingSurface1.Size = new System.Drawing.Size(1429, 448);
+			this.drawingSurface1.TabIndex = 2;
+			this.drawingSurface1.Text = "drawingSurface1";
+			// 
+			// hScrollBar1
+			// 
+			this.hScrollBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.hScrollBar1.Location = new System.Drawing.Point(0, 452);
+			this.hScrollBar1.Name = "hScrollBar1";
+			this.hScrollBar1.Size = new System.Drawing.Size(1429, 21);
+			this.hScrollBar1.TabIndex = 3;
+			// 
+			// vScrollBar1
+			// 
+			this.vScrollBar1.Dock = System.Windows.Forms.DockStyle.Right;
+			this.vScrollBar1.Location = new System.Drawing.Point(1412, 25);
+			this.vScrollBar1.Name = "vScrollBar1";
+			this.vScrollBar1.Size = new System.Drawing.Size(17, 427);
+			this.vScrollBar1.TabIndex = 4;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1429, 473);
+			this.Controls.Add(this.vScrollBar1);
+			this.Controls.Add(this.hScrollBar1);
+			this.Controls.Add(this.drawingSurface1);
+			this.Controls.Add(this.toolStrip1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripDropDownButton DiagramsDropDownBox;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem arrayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pointerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripDropDownButton UMLDropDownBox;
+        private System.Windows.Forms.ToolStripMenuItem classToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem relationshipToolStripMenuItem;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripMenuItem insert_Rectangle;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+		private System.ComponentModel.BackgroundWorker backgroundWorker2;
+		private DrawingSurface drawingSurface1;
+		private System.Windows.Forms.HScrollBar hScrollBar1;
+		private System.Windows.Forms.VScrollBar vScrollBar1;
+	}
+}
+
---
+++ Testing Grounds/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Drawing.Drawing2D.GraphicsPath;
+using System.Drawing.Drawing2D;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        PaintEventArgs PEA;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripDropDownButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void arrayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insert_Rectangle_Click(object sender, EventArgs e)
+        {
+			//PaintEventArgs pea = new PaintEventArgs();
+
+			//  Rectangle box = new Rectangle(250, 0, 100, 200);
+
+			// paint_Rectangle(box);
+
+			Point p = new Point(100, 100);
+
+			Box box = new Box(p);
+
+			drawingSurface1.Shapes.Add(box);
+
+			box.Draw(drawingSurface1.CreateGraphics());
+        }
+
+        private void paint_Rectangle(Rectangle box)
+        {
+            Pen pen = new Pen(Color.Black, 2);
+
+            
+
+            PEA.Graphics.DrawRectangle(pen, box);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            PEA = e;
+			Rectangle rect = new Rectangle(0, 0, 200, 100);
+            paint_Rectangle(rect);
+        }
+    }
+
+    public interface Item
+    {
+        GraphicsPath getPath();
+        bool HitTest(Point p);
+        void Draw(Graphics g);
+        void Move(Point p);
+    }
+
+    public class Box : Item
+    {
+        public Box(Point p)
+        {
+            height = 50;
+            width = 100;
+            color = Color.Black;
+            thickness = 5;
+			TLCorner = p;
+        }
+        public int height { get; set; }
+        public int width { get; set; }
+        public Point TLCorner { get; set; } //Top Left Corner of box
+        public Color color { get; set; }
+        public int thickness { get; set; } //thickness of border lines
+
+        public GraphicsPath getPath()
+        {
+            var path = new GraphicsPath();
+
+            Rectangle rect = new Rectangle(TLCorner.X, TLCorner.Y, width, height);
+
+            path.AddRectangle(rect);
+            return path;
+        }
+
+        public bool HitTest(Point p)
+        {
+            bool result = false;
+
+            using (var path = getPath()) {
+                //the "using" statement in this context seems to be a memory management tool
+                //and the statement expands into a try-finally block (a try-catch block with no catch block, but a finally block that executes regardless of exceptions)
+
+                //At the end of the "using" statement, it calls "Dispose" on the variable declared or specified in the parentheses.
+                //look-up IDisposable for more info
+                result = path.IsVisible(p);
+            }
+            return result;
+        }
+
+        public void Draw(Graphics g)
+        {
+           // using (var path = getPath())
+           // {
+                using (var pen = new Pen(color, thickness))
+                {
+                    g.DrawRectangle(pen, new Rectangle(TLCorner.X, TLCorner.Y, width, height));
+                }
+           // }
+        }
+
+        public void Move(Point p)
+        {
+            TLCorner = new Point(TLCorner.X + p.X, TLCorner.Y + p.Y);
+        }
+    }
+
+    public class DrawingSurface : Control
+    {
+        public List<Item> Shapes { get; private set; }
+        Item selectedShape;
+        bool moving;
+		Point previousPoint = Point.Empty;
+
+		public DrawingSurface(){
+			DoubleBuffered = true;
+			Shapes = new List<Item>();
+		}
+
+		protected override void OnMouseDown(MouseEventArgs e)
+		{
+			for(var i = Shapes.Count - 1; i >= 0; i--)
+			{
+				if (Shapes[i].HitTest(e.Location))
+				{
+					selectedShape = Shapes[i];
+					break;
+				}
+			}
+
+			if(selectedShape != null)
+			{
+				moving = true; previousPoint = e.Location;
+			}
+
+			base.OnMouseDown(e);
+		}
+
+		protected override void OnMouseMove(MouseEventArgs e)
+		{
+			if (moving)
+			{
+				var p = new Point(e.X - previousPoint.X, e.Y - previousPoint.Y);
+				selectedShape.Move(p);
+				previousPoint = e.Location;
+				this.Invalidate();
+			}
+
+			base.OnMouseMove(e);
+		}
+
+		protected override void OnMouseUp(MouseEventArgs e)
+		{
+			if(moving)
+			{
+				selectedShape = null;
+				moving = false;
+			}
+
+			base.OnMouseUp(e);
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+			foreach(var shape in Shapes)
+			{
+				shape.Draw(e.Graphics);
+			}
+
+			base.OnPaint(e);
+		}
+	}
+}
